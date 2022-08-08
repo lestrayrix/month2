3ee1ef154729938d2b19208d2cc91cdaaab50241
lestrayrix <starkvinlovebts@gmail.com>
2022-08-08 17:01:23 +0300

    dz

---
+++ HW1_17-01_Jyldyz/greeting.js
@@ -0,0 +1,19 @@
+const name = prompt("Ваше имя?")
+
+alert("Привет ,"+name+"!")
+
+const arr =["Rebekah",true,false,34]
+const arr2 =["Rebekah",true,false,34]
+console.log(arr.length)
+console.log(arr2.length)
+
+if (arr<arr2){
+    alert("arr меньше arr2")
+}else if (arr>arr2) {
+    alert("arr больше arr2")
+}else{
+    alert("они равны")
+} 
+
+
+
